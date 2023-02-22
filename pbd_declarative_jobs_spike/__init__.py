0c58cf67ee8e166c114052b190ac604e94d9a824
Dave Randall <19395688+daveygit2050@users.noreply.github.com>
2023-02-22 14:28:23 +0000

    PBD-4085 Generate pipeline and sbt build job DSL
    Co-authored-by: Steve Reddel <1107842+sreddel@users.noreply.github.com>
    Co-authored-by: Andy Waddams <61849414+waddamski@users.noreply.github.com>

--- pbd_declarative_jobs_spike/__init__.py
+++ pbd_declarative_jobs_spike/__init__.py
@@ -1,33 +1,76 @@
 import hashlib
 
+from typing import List
+
 import yaml
 
 def run_cli():
     service_name = "my-lovely-service" # This would be the repo name from the GitHub event
     with open(f"repository_yaml/{service_name}/repository.yaml", "r") as repository_yaml_file:
         repository_config = yaml.safe_load(repository_yaml_file)
-    print(repository_config)
     build_folder = repository_config['build']['folder']
     folder_hash = hashlib.md5(build_folder.encode('utf-8')).hexdigest()
-    folder_file_name = f"{folder_hash}.groovy"
-    job_file_name = f"{folder_hash}-{service_name}.groovy"
-    print(f"Please save the folder DSL to {folder_file_name}")
-    print(f"Please save the job DSL to {job_file_name}")
+    folder_file_name = f"dsl_{folder_hash}.groovy"
+    service_file_name = f"dsl_{folder_hash}_{service_name.replace('-', '_')}.groovy"
     build_folders = build_folder.split('/')
-    print(build_folders)
-    folder_dsl = "import uk.gov.hmrc.buildjobs.domain.builder.TeamFolderBuilder\n\n"
+    folder_dsl = (
+        "import javaposse.jobdsl.dsl.DslFactory\n\n"
+        "import uk.gov.hmrc.buildjobs.domain.builder.TeamFolderBuilder\n\n"
+    )
     for depth in range(len(build_folders)):
         folder_dsl = folder_dsl + get_team_folder_builder(
             fully_qualified_folder_name="/".join(build_folders[0:depth+1]),
             folder_name=build_folders[depth],
         )
+    # We would really generate only the imports required
+    service_dsl = (
+        "import javaposse.jobdsl.dsl.DslFactory\n\n"
+        "import uk.gov.hmrc.buildjobs.domain.builder.PipelineJobBuilder\n"
+        "import uk.gov.hmrc.buildjobs.domain.builder.SbtMicroserviceJobBuilder\n"
+        "import static uk.gov.hmrc.buildjobs.domain.builder.pipeline.Environments.INTEGRATION\n"
+        "import static uk.gov.hmrc.buildjobs.domain.builder.pipeline.Environments.DEVELOPMENT\n"
+        "import static uk.gov.hmrc.buildjobs.domain.builder.pipeline.Environments.QA\n"
+        "import static uk.gov.hmrc.buildjobs.domain.builder.pipeline.Environments.STAGING\n\n"
+    )
+    service_dsl = service_dsl + get_pipeline_job_builder(
+        fully_qualified_folder_name=build_folder,
+        service_name=service_name,
+        environments=repository_config["environments"]
+    )
+    service_dsl = service_dsl + get_sbt_micorservice_job_builder(
+        fully_qualified_folder_name=build_folder,
+        service_name=service_name,
+        mongo=repository_config.get("mongo")
+    )
     with open(f"build/{folder_file_name}", "w") as folder_file:
         folder_file.write(folder_dsl)
+    with open(f"build/{service_file_name}", "w") as folder_file:
+        folder_file.write(service_dsl)
 
 
 def get_team_folder_builder(fully_qualified_folder_name: str, folder_name=str) -> str:
     return (
         f"new TeamFolderBuilder(\"{fully_qualified_folder_name}\")\n"
         f"   .withDisplayName(\"{folder_name}\")\n"
-        "   .build(this as DslFactory)\n\n"
+        f"   .build(this as DslFactory)\n\n"
     )
+
+
+def get_pipeline_job_builder(fully_qualified_folder_name: str, service_name: str, environments: List[str]):
+    dsl = (
+        f"new PipelineJobBuilder(\"{fully_qualified_folder_name}\", \"{service_name}-pipeline\")\n"
+        f"   .buildMicroservice(\"{service_name}\")\n"
+    )
+    for environment in environments:
+        dsl = dsl + f"   .andThenDeployTo({environment.upper()})\n"
+    dsl = dsl + f"   .build(this as DslFactory)\n\n"
+    return dsl
+
+
+def get_sbt_micorservice_job_builder(fully_qualified_folder_name: str, service_name: str, mongo: bool):
+    dsl = f"new SbtMicroserviceJobBuilder(\"{fully_qualified_folder_name}\", \"{service_name}\")\n"
+    if mongo:
+        dsl = dsl + "   .withMongo()\n"
+    dsl = dsl + "   .build(this as DslFactory)\n\n"
+    return dsl
+
